1d43321a560c6c541a3ec30b22687f58eeb0f527
KapitaZzz <agarussky@gmail.com>
2022-06-09 22:36:27 +0300

    1. Quartz [#175122 #310253]

--- src/main/java/quartz/AlertRabbit.java
+++ src/main/java/quartz/AlertRabbit.java
@@ -2,18 +2,24 @@ package quartz;
 
 import org.quartz.*;
 import org.quartz.impl.StdSchedulerFactory;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
 import static org.quartz.JobBuilder.*;
 import static org.quartz.TriggerBuilder.*;
 import static org.quartz.SimpleScheduleBuilder.*;
 
 public class AlertRabbit {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         try {
             Scheduler scheduler = StdSchedulerFactory.getDefaultScheduler();
             scheduler.start();
             JobDetail job = newJob(Rabbit.class).build();
             SimpleScheduleBuilder times = simpleSchedule()
-                    .withIntervalInSeconds(10)
+                    .withIntervalInSeconds(setInterval("rabbit.properties"))
                     .repeatForever();
             Trigger trigger = newTrigger()
                     .startNow()
@@ -22,6 +28,14 @@ public class AlertRabbit {
             scheduler.scheduleJob(job, trigger);
         } catch (SchedulerException se) {
             se.printStackTrace();
+        }
+    }
+
+    public static int setInterval(String props) throws IOException {
+        try (InputStream inputStream = AlertRabbit.class.getClassLoader().getResourceAsStream(props)) {
+            Properties properties = new Properties();
+            properties.load(inputStream);
+            return Integer.parseInt(properties.getProperty(props));
         }
     }
 
