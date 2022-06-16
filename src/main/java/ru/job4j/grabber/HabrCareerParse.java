b5a998bb9de00f67b4bd3ed633e7188d0480505b
KapitaZzz <agarussky@gmail.com>
2022-06-16 22:13:21 +0300

    1.1. Job c параметрами [#260360]

---
+++ src/main/java/ru/job4j/grabber/HabrCareerParse.java
@@ -0,0 +1,35 @@
+package ru.job4j.grabber;
+
+import org.jsoup.Connection;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+
+public class HabrCareerParse {
+
+    private static final String SOURCE_LINK = "https://career.habr.com";
+
+    private static final String PAGE_LINK = String.format("%s/vacancies/java_developer", SOURCE_LINK);
+
+    public static void main(String[] args) throws IOException {
+        Connection connection = Jsoup.connect(PAGE_LINK);
+        Document document = connection.get();
+        Elements rows = document.select(".vacancy-card__inner");
+        Elements dateRows = document.select(".vacancy-card__date");
+        rows.forEach(row -> {
+            Element titleElement = row.select(".vacancy-card__title").first();
+            Element linkElement = titleElement.child(0);
+            Element dateTitleElement = dateRows.select(".basic-date").first();
+            Element dateLinkElement = dateTitleElement.child(0);
+            String vacancyName = titleElement.text();
+            String link = String.format("%s%s", SOURCE_LINK, linkElement.attr("href"));
+            String date = String.format("%s%s", SOURCE_LINK, dateLinkElement.attr("datetime"));
+            System.out.printf("%s %s %s%n", vacancyName, link, date);
+        });
+    }
+}
+
+
