3cb6d19eed815dd57da83f15d4743033b1b3276b
KapitaZzz <agarussky@gmail.com>
2022-06-21 19:41:55 +0300

    2.1. Преобразование даты [#289476]

--- src/main/java/ru/job4j/grabber/utils/HabrCareerDateTimeParser.java
+++ src/main/java/ru/job4j/grabber/utils/HabrCareerDateTimeParser.java
@@ -6,7 +6,6 @@ import java.time.ZonedDateTime;
 public class HabrCareerDateTimeParser implements DateTimeParser {
     @Override
     public LocalDateTime parse(String time) {
-        String[] dateTimeArr = time.split("T");
-        return ZonedDateTime.parse(dateTimeArr[0]).toLocalDateTime();
+        return ZonedDateTime.parse(time).toLocalDateTime();
     }
 }
