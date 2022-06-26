ad866980f516c58756f2ef91b64aba0ef302a784
KapitaZzz <agarussky@gmail.com>
2022-06-26 15:13:15 +0300

    2.1.1. Парсинг https://www.sql.ru/forum/job-offers/3 [#285210]

--- src/main/java/ru/job4j/grabber/HabrCareerParse.java
+++ src/main/java/ru/job4j/grabber/HabrCareerParse.java
@@ -5,6 +5,7 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
+import ru.job4j.grabber.utils.HabrCareerDateTimeParser;
 
 import java.io.IOException;
 
@@ -15,20 +16,17 @@ public class HabrCareerParse {
     private static final String PAGE_LINK = String.format("%s/vacancies/java_developer", SOURCE_LINK);
 
     public static void main(String[] args) throws IOException {
-        Connection connection = Jsoup.connect(PAGE_LINK);
-        Document document = connection.get();
-        Elements rows = document.select(".vacancy-card__inner");
-        Elements dateRows = document.select(".vacancy-card__date");
-        rows.forEach(row -> {
-            Element titleElement = row.select(".vacancy-card__title").first();
-            Element linkElement = titleElement.child(0);
-            Element dateTitleElement = dateRows.first().child(0);
-            String vacancyName = titleElement.text();
-            String link = String.format("%s%s", SOURCE_LINK, linkElement.attr("href"));
-            System.out.printf("%s %s %s%n", vacancyName, link, dateTitleElement.attr("datetime"));
-        });
+        for (int i = 0; i <= 5; i++) {
+            Document document = Jsoup.connect("https://career.habr.com/vacancies/java_developer?page=" + i).get();
+            Elements rows = document.select(".vacancy-card__inner");
+            for (Element element : rows) {
+                HabrCareerDateTimeParser parser = new HabrCareerDateTimeParser();
+                element.children().get(2).text();
+                parser.parse(element.children().get(0).children().get(0).attr("datetime"));
+
+            }
+        }
     }
 }
-
 
 
