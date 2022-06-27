9059cd7875d1c641a7f28968159bfaf3ffef27a2
KapitaZzz <agarussky@gmail.com>
2022-06-27 12:29:33 +0300

    2.1.1. Парсинг https://www.sql.ru/forum/job-offers/3 [#285210 #316505]

--- src/main/java/ru/job4j/grabber/HabrCareerParse.java
+++ src/main/java/ru/job4j/grabber/HabrCareerParse.java
@@ -16,14 +16,15 @@ public class HabrCareerParse {
     private static final String PAGE_LINK = String.format("%s/vacancies/java_developer", SOURCE_LINK);
 
     public static void main(String[] args) throws IOException {
-        for (int i = 0; i <= 5; i++) {
+        for (int i = 1; i <= 5; i++) {
             Document document = Jsoup.connect("https://career.habr.com/vacancies/java_developer?page=" + i).get();
             Elements rows = document.select(".vacancy-card__inner");
             for (Element element : rows) {
                 HabrCareerDateTimeParser parser = new HabrCareerDateTimeParser();
-                element.children().get(2).text();
-                parser.parse(element.children().get(0).children().get(0).attr("datetime"));
-
+                System.out.println("https://career.habr.com" + element.children().get(1).attr("href"));
+                System.out.println(element.children().get(2).text());
+                System.out.println(parser
+                        .parse(element.children().get(0).children().get(0).attr("datetime")));
             }
         }
     }
